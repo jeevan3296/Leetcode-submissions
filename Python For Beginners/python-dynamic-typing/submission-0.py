46a7ebce65adee573be8abff36019f855003e486
jeevan3296 <56495586+jeevan3296@users.noreply.github.com>
2026-05-05 16:54:31 -0700

    Add: python-dynamic-typing - submission-0

---
+++ Python For Beginners/python-dynamic-typing/submission-0.py
@@ -0,0 +1,14 @@
+variable = 10
+print(type(variable))
+
+variable = 10.1
+print(type(variable))
+
+variable = False
+print(type(variable))
+
+variable = "jeevan"
+print(type(variable))
+
+variable = [1, 2, 3]
+print(type(variable))

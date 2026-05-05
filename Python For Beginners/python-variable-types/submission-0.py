647ffd71ad0baa3ca9c99b6fcfa975a440677867
jeevan3296 <56495586+jeevan3296@users.noreply.github.com>
2026-05-05 16:50:12 -0700

    Add: python-variable-types - submission-0

---
+++ Python For Beginners/python-variable-types/submission-0.py
@@ -0,0 +1,15 @@
+integer_type = 10
+
+float_type = 10.1
+
+bool_type = True
+
+string_type = "Jeevan"
+
+list_type = [1, 2, 3, 4, 5]
+
+print(type(integer_type))
+print(type(float_type))
+print(type(bool_type))
+print(type(string_type))
+print(type(list_type))
